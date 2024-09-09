5fc3d3f285b2ebfbb6411b24451ddbd6975a9843
Dominic Visco <dom@suitable.co>
2024-09-09 15:37:22 -0400

    Integrated with engage create event api

--- anthology-engage-events/src/functions/push-events.js
+++ anthology-engage-events/src/functions/push-events.js
@@ -6,15 +6,17 @@ function printAxiosError(error) {
 }
 
 /**
- * Pushes events from suitable to engage by reading all the available events from suitable and
- * all the available events from engage. Then pushes any events that are missing from the two.
+ * Pushes events from suitable to engage by reading all the available events from suitable from a certain point in time
+ * and then pushes these events to an Organization's event calendar in Engage.
  *
  * @param suitableUsername
  * @param suitablePassword
  * @param suitablePathwayId
  * @param suitableInstitutionId
  * @param suitableBaseUrl
+ * @param suitableDateCreatedDaysAgoOrLess
  * @param engageApiKey
+ * @param engageApiBaseUrl
  * @param engageSubmittedByOrganizationId
  * @param engageSubmittedByIdCampusEmail
  */
@@ -24,7 +26,9 @@ async function pushEventsFromSuitable({
     suitablePathwayId,
     suitableInstitutionId,
     suitableBaseUrl,
+    suitableDateCreatedDaysAgoOrLess,
     engageApiKey,
+    engageApiBaseUrl,
     engageSubmittedByOrganizationId,
     engageSubmittedByIdCampusEmail,
 }) {
@@ -36,8 +40,14 @@ async function pushEventsFromSuitable({
         }
     });
 
-    // // Read an environment variable (if needed) or simply print "Hello, World!"
-    // const message = process.env.MESSAGE || "Hello, World!";
+    const engageApi = axios.create({
+        baseURL: engageApiBaseUrl,
+        headers: {
+            'Accept': 'application/json',
+            'X-Engage-Api-Key': engageApiKey,
+        }
+    });
+
     console.log("Attempting to pull events from suitable using")
     console.log("suitableUsername", suitableUsername)
     console.log("suitablePassword", suitablePassword)
@@ -47,6 +57,7 @@ async function pushEventsFromSuitable({
     console.log("engageSubmittedByIdCampusEmail", engageSubmittedByIdCampusEmail)
 
     try {
+        // Login API from https://developer.suitable.co/#00809ff1-90fd-40a0-8cae-4575fe24d9eb
         const loginResponse = await suitableApi.post('/v1/auth/login',{
             username: suitableUsername,
             password: suitablePassword,
@@ -56,10 +67,23 @@ async function pushEventsFromSuitable({
         const suitableAuthHeader = {
             'Authorization': `Bearer ${accessToken}`
         }
-        const baseParams = {
-            date: new Date().getTime(),
-            direction: 'asc',
+
+        // List Events API from https://developer.suitable.co/#308592d2-5f76-4cf7-a30f-ed16fc5363d9
+        const now = new Date().getTime()
+        let dateRestriction = {
+            date: now,
             orderBy: 'proximityDate',
+        }
+        if (suitableDateCreatedDaysAgoOrLess) {
+            dateRestriction = {
+                date: now - (suitableDateCreatedDaysAgoOrLess * 24 * 60 * 60 * 1000),
+                orderBy: 'dateCreated',
+            }
+        }
+        console.log("Using date restriction", dateRestriction)
+        const baseParams = {
+            ...dateRestriction,
+            direction: 'asc',
             types: 'EVENT',
         }
         let cursor = null
@@ -89,16 +113,43 @@ async function pushEventsFromSuitable({
             await fetchEvents(cursor)
         }
 
-        // TODO: Read events from Engage
-        // TODO: Filter out events from suitable that exist in Engage
-        // TODO: Create any missing event in Engage
+        // Ideally we could read events from Engage + filter out events from suitable that exist in Engage but Engage
+        // offers no way to include a reference ID on an event there isn't a great way to confidently look for
+        // events we already created and exclude those. Instead, we aim to only create events that were included from
+        // the date created API predicate.
+        for (let suitableEvent of suitableEvents) {
+            // console.log("Pushing event", suitableEvent)
 
+            // Used Engage's Create Event API https://engage-api.campuslabs.com/swagger/index.html#/Events/post_v3_0_events_event
+            await engageApi.post("/v3.0/events/event", {
+                submittedByOrganizationId: engageSubmittedByOrganizationId,
+                submittedById: {
+                    campusEmail: engageSubmittedByIdCampusEmail,
+                },
+                name: suitableEvent.title,
+                description: suitableEvent.description,
+                startsOn: new Date(suitableEvent.startDate).toISOString(),
+                endsOn: new Date(suitableEvent.endDate).toISOString(),
+                address: {
+                    onlineLocation: `${suitableBaseUrl}/institution/${suitableInstitutionId}/activities/${suitableEvent.id}`,
+                    instructions: "To obtain credit for this event, log into Suitable",
+                },
+                benefits: [ "Credit" ],
+                state: {
+                    status: "Approved",
+                },
+                visibility: "Organization",
+            })
+        }
+
+        return suitableEvents
     } catch (error) {
         if (error instanceof axios.AxiosError) {
             printAxiosError(error)
         } else {
             console.error('Error processing:', error);
         }
+        return null
     }
 }
 
--- anthology-engage-events/src/index.js
+++ anthology-engage-events/src/index.js
@@ -5,7 +5,9 @@ const suitablePassword = process.env["SUITABLE_PASSWORD"]
 const suitablePathwayId = process.env["SUITABLE_PATHWAY_ID"]
 const suitableInstitutionId = process.env["SUITABLE_INSTITUTION_ID"]
 const suitableBaseUrl = process.env["SUITABLE_BASE_URL"] || "https://app.suitable.co"
+const suitableDateCreatedDaysAgoOrLess = process.env["SUITABLE_DATE_CREATED_DAYS_AGO_OR_LESS"]
 const engageApiKey = process.env["ENGAGE_API_KEY"]
+const engageApiBaseUrl = "https://engage-api.campuslabs.com/api"
 const engageSubmittedByOrganizationId = process.env["ENGAGE_SUBMITTED_BY_ORGANIZATION_ID"]
 const engageSubmittedByIdCampusEmail = process.env["ENGAGE_SUBMITTED_BY_ID_CAMPUS_EMAIL"]
 
@@ -15,7 +17,13 @@ pushEventsFromSuitable({
     suitablePathwayId,
     suitableInstitutionId,
     suitableBaseUrl,
+    suitableDateCreatedDaysAgoOrLess: suitableDateCreatedDaysAgoOrLess ? parseInt(suitableDateCreatedDaysAgoOrLess) : null,
     engageApiKey,
-    engageSubmittedByOrganizationId,
+    engageApiBaseUrl,
+    engageSubmittedByOrganizationId: parseInt(engageSubmittedByOrganizationId),
     engageSubmittedByIdCampusEmail,
+}).then((events) => {
+    if (events) {
+        console.log(`Finished pushing ${events.length} events to engage`)
+    }
 })
